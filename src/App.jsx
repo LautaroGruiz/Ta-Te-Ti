406063679e61b2c8cb86d0bc459e399019004d9d
Lautaro Gonzalez Ruiz <lautawork@gmail.com>
2023-09-02 17:34:22 -0300

    Logica desarrollada para detectar un ganador

--- src/App.jsx
+++ src/App.jsx
@@ -17,15 +17,41 @@ const Square = ({ children, isSelected, upDateBoard, index }) => {
   );
 };
 
+const winnerCombos = [
+  [0, 1, 2],
+  [3, 4, 5],
+  [6, 7, 8],
+  [0, 3, 6],
+  [1, 4, 7],
+  [2, 5, 8],
+  [0, 4, 8],
+  [2, 4, 6],
+];
+
 const App = () => {
   const [board, setBoard] = useState(Array(9).fill(null));
   const [turn, setTurn] = useState(TURNS.X);
   const [winner, setWinner] = useState(null);
 
+  const checkWinner = (boardToCheck) => {
+    // Recorremos el array winnerCombos y traemos sus valores.
+    // Checkear si hay in ganador
+    for (const combo of winnerCombos) {
+      const [a, b, c] = combo;
+      if (
+        boardToCheck[a] &&
+        boardToCheck[a] === boardToCheck[b] &&
+        boardToCheck[a] === boardToCheck[c]
+      ) {
+        return boardToCheck[a];
+      }
+    }
+    return null;
+  };
+
   const upDateBoard = (index) => {
     // No actualizar la posicion si ya tiene marca.
-    // Comentario para push
-    if (board[index]) return;
+    if (board[index] ||  winner) return;
     // Clonamos los datos del estado del tablero para crear un nuevo estado y actualizar.
     const newBoard = [...board];
     newBoard[index] = turn;
@@ -33,6 +59,11 @@ const App = () => {
     // Dependiendo del tipo de turno, sigue el otro.
     const newTurns = turn === TURNS.X ? TURNS.O : TURNS.X;
     setTurn(newTurns);
+    // Revisar si hay ganador
+    const newWinner = checkWinner(newBoard)
+    if (newWinner) {
+      setWinner(newWinner);
+    }
   };
 
   return (
